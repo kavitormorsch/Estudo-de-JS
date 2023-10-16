688ceece4d43884e3965e4fcc53d4e49843904f4
kavitormorsch <kavitormorsch@gmail.com>
2023-10-16 12:33:13 -0300

    atualizada a aula59

--- aula59 - aula57 porém com Constructor Functions/assets/js/main.js
+++ aula59 - aula57 porém com Constructor Functions/assets/js/main.js
@@ -1,65 +1,61 @@
-function createCalculator(){
+function Calculator(){
+    this.display = document.querySelector('.display');
     
-    return {
-        display: document.querySelector('.display'),
+    this.start = function(){
+        console.log(this.display);
+        this.clickbuttons();
+    }
 
-        start(){
-            console.log(this.display);
-            this.clickbuttons();
-        },
+    this.clearDisplay = function(){
+        this.display.value = '';
+    }
 
-        clearDisplay(){
-            this.display.value = '';
-        },
+    this.deleteOne = function(){
+        this.display.value = this.display.value.slice(0, -1);
+    }
 
-        deleteOne(){
-            this.display.value = this.display.value.slice(0, -1);
-        },
-
-        doOperation(){
-            let operation = this.display.value;
-            try {
-                operation = eval(operation);
-                if(!operation) {
-                    alert('Invalid operation');
-                    return;
-                }
-                this.display.value = String(operation);
-            } catch(e){
+    this.doOperation = function(){
+        let operation = this.display.value;
+        try {
+            operation = eval(operation);
+            if(!operation) {
                 alert('Invalid operation');
                 return;
             }
-        },
-        clickbuttons(){
-            document.addEventListener('click', e => {
-                const el = e.target;
+            this.display.value = String(operation);
+        } catch(e){
+            alert('Invalid operation');
+            return;
+        }
+    };
+    this.clickbuttons = function(){
+        document.addEventListener('click', e => {
+            const el = e.target;
 
-                if(el.classList.contains('btn-num')) {
-                    this.btnParaDisplay(el.innerText);
-                }
-                if(el.classList.contains('btn-clear')){
-                    this.clearDisplay();
-                }
-                if(el.classList.contains('btn-del')){
-                    this.deleteOne();
-                }
-                if(el.classList.contains('btn-eq')){
-                    this.doOperation();
-                }
-            });
-            this.display.addEventListener('keyup', e => {
-                if(e.keyCode === 13){
-                    this.doOperation();
-                }
-            });
-        },
-
-        btnParaDisplay(buttonNumber){
-            this.display.value += buttonNumber;
-        },
+            if(el.classList.contains('btn-num')) {
+                this.btnParaDisplay(el.innerText);
+            }
+            if(el.classList.contains('btn-clear')){
+                this.clearDisplay();
+            }
+            if(el.classList.contains('btn-del')){
+                this.deleteOne();
+            }
+            if(el.classList.contains('btn-eq')){
+                this.doOperation();
+            }
+        });
+        this.display.addEventListener('keyup', e => {
+            if(e.keyCode === 13){
+                this.doOperation();
+            }
+        });
     };
 
-}
+    this.btnParaDisplay = function(buttonNumber){
+        this.display.value += buttonNumber;
+    };
+};
 
 const calculator = new Calculator();
 calculator.start();
